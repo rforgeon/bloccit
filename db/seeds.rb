45697d7b083c56df23350090b3f930096206a478
rforgeon <rforgeon@gmail.com>
2016-10-18 08:19:50 -0700

    assignment-17-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,13 @@ require 'random_data'
   )
 end
 
+def foundPost
+  post = Post.find_or_create_by!(title: "Manual Seed Post") do |post|
+    post.body = "This is a manually seeded body. Beep, boop, bop."
+  end
+  return post
+end
+
 posts = Post.all
 
 100.times do
@@ -16,6 +23,10 @@ posts = Post.all
   )
 end
 
+Comment.find_or_create_by!(post: foundPost) do |comment|
+  comment.body = "This is a manually comment body. Bop, boop, beep."
+end
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
-puts "#{Comment.count} posts created"
+puts "#{Comment.count} comments created"
