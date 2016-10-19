e01a1e2b701d6d73173689143fa480993bb49e01
rforgeon <rforgeon@gmail.com>
2016-10-19 13:47:18 -0700

    assignment-18-crud Completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    spamFilter
   end
 
   def show
@@ -11,4 +12,17 @@ class PostsController < ApplicationController
 
   def edit
   end
+
+  def spamFilter
+    @posts = Post.all
+    count = 0
+    @posts.each do |spam|
+      if count % 5 == 0
+        spam.title = "SPAM"
+        spam.save
+      end
+      count = count+1
+    end
+  end
+
 end
