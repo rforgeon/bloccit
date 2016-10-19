20d1db9afc65fa84f24c198ca3ccd6e2455fff68
rforgeon <rforgeon@gmail.com>
2016-10-19 09:38:52 -0700

    assignment-18-crud in-progress

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
 class Post < ActiveRecord::Base
   has_many :comments
+
+  post = Post.all
+  count = 0
+  post.each do |spam|
+    if count % 5 == 0
+      spam.title = "SPAM"
+      spam.save
+    end
+    count = count+1
+  end
+
 end
