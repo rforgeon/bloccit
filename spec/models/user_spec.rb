7d0248a9aafec06fed86565abf2f0d1ed5aac182
rforgeon <rforgeon@gmail.com>
2016-10-27 12:46:20 -0700

    checkpoint-24-auth-completed2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  let(:user) {User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password")}
-
+  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+  # Shoulda tests for name
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
 
-  it {is_expected.to validate_presence_of(:email)}
-  it {is_expected.to validate_uniqueness_of(:email)}
+  #email
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to validate_length_of(:email).is_at_least(3) }
   it { is_expected.to allow_value("user@bloccit.com").for(:email) }
 
+  #password
   it { is_expected.to validate_presence_of(:password) }
   it { is_expected.to have_secure_password }
   it { is_expected.to validate_length_of(:password).is_at_least(6) }
@@ -23,17 +25,17 @@ RSpec.describe User, type: :model do
   end
 
   describe "invalid user" do
-   let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
-   let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
+     let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
+     let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
 
-   it "should be an invalid user due to blank name" do
-     expect(user_with_invalid_name).to_not be_valid
+     it "should be an invalid user due to blank name" do
+       expect(user_with_invalid_name).to_not be_valid
+     end
+
+     it "should be an invalid user due to blank email" do
+       expect(user_with_invalid_email).to_not be_valid
+     end
+
    end
-
-   it "should be an invalid user due to blank email" do
-     expect(user_with_invalid_email).to_not be_valid
-   end
-
- end
 
 end
