31eda3ce5ef93a8ebd8f52d3d4289921c15b71e4
rforgeon <rforgeon@gmail.com>
2016-10-27 13:03:12 -0700

    assignment-24-auth-completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ActiveRecord::Base
 
   before_save { self.email = email.downcase if email.present? }
+  before_save { capName if name.present?}
 
-  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+ def capName
+   nameArray = self.name
+   nameArray = name.split(' ')
+   nameArray.each do |f|
+     f.capitalize!
+   end
+   self.name = nameArray.join(" ")
+ end
+
+ validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
  validates :password, length: { minimum: 6 }, allow_blank: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe User, type: :model do
   # Shoulda tests for name
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
+  it { is_expected.to allow_value("Bloccit User").for(:name) }
 
   #email
   it { is_expected.to validate_presence_of(:email) }
