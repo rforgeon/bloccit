4170df27e3c8b4868ff4d249fb7659fc131a8921
rforgeon <rforgeon@gmail.com>
2016-10-31 14:26:04 -0700

    checkpoint-32-profile-completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe Post, type: :model do
 
   let(:name) { RandomData.random_sentence }
    let(:description) { RandomData.random_paragraph }
-   let(:topic) { RandomData.random_sentence }
 
-   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
-   let(:post) { topic.posts.create!(title: title, body: body, user: user) }
+   let(:topic) { create(:topic) }
+   let(:user) { create(:user) }
+   let(:post) { create(:post) }
 
    it { is_expected.to have_many(:comments) }
    it { is_expected.to belong_to(:topic) }
@@ -25,7 +25,7 @@ RSpec.describe Post, type: :model do
 
   describe "attributes" do
     it "has a title, body attribute" do
-         expect(post).to have_attributes(title: title, body: body, user: user)
+      expect(post).to have_attributes(title: post.title, body: post.body)
     end
   end
 
