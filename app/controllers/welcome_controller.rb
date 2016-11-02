2975d585b02cabc44d2dad05846ac0b446fa73bd
rforgeon <rforgeon@gmail.com>
2016-11-02 12:52:12 -0700

    video added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,29 @@
 class WelcomeController < ApplicationController
+
+  helper_method :get_link_info
+  helper_method :strip_link_id
+
   def index
   end
 
   def about
   end
 
-  def faq
+  def get_link_info(link)
+    link_obj = LinkThumbnailer.generate(link)
+    return link_obj
+  end
+
+  def strip_link_id(link)
+    link.reverse!
+    if link.include? '='
+      codeArray = link.split('=',2)
+    else
+      codeArray = link.split('/',2)
+    end
+    code = codeArray[0]
+    code.reverse!
+    return code
   end
 
 end
